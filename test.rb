80bbb7d4ae4e5dbbcef0f9cac6084118adc14dd4
Akbar <muhrizkiakbar@live.com>
2021-09-17 23:45:55 +0800

    fibonacci

--- test.rb
+++ test.rb
@@ -2,16 +2,15 @@ require 'pry'
 require 'rspec'
 
 class String
-  def self.palindrome?(str)
-    str = str.to_s
+  def palindrome?
     str_reversed = ""
-    str_length = str.length - 1
+    str_length = self.length - 1
 
     str_length.downto(0).each do |s|
-      str_reversed << str[s]
+      str_reversed << self[s]
     end
 
-    return false unless str == str_reversed
+    return false unless self == str_reversed
 
     true
   end
@@ -24,7 +23,23 @@ RSpec.describe "Gredu" do
     end
 
     it "result false with value gambar" do
-      expect("samas".palindrome?).to eq(true)
+      expect("gambar".palindrome?).to eq(false)
     end
   end
 end
+
+$counter = 0
+def fibonacci(number_index)
+  puts "jalan" + number_index.to_s
+  puts "stop " + number_index.to_s if (0..1).include? number_index
+  return number_index if (0..1).include? number_index
+  $counter += 1
+  puts "*" * $counter
+  puts "kalian ke " + $counter.to_s
+  puts "hasil pengurangan 1 = " + (number_index - 1).to_s
+  puts "hasil pengurangan 2 = " + (number_index - 2).to_s
+  ( fibonacci(number_index - 1) + fibonacci(number_index - 2) )
+end
+
+puts fibonacci(3)
+
